ebcc3c63ed607cc887141b37c60b6d92161b4f86
KurtSalonga <kasalonga23-0391@cca.edu.ph>
2025-02-26 19:38:15 +0800

    UPDATED PHP FORM FILE
    UPDATED REGISTER.PHP AND ACTION_PAGE.PHP FILE

--- action_page.php
+++ action_page.php
@@ -1,41 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>REGISTER FORM</title>
-</head>
-<body>
-    <h1>REGISTER HERE!</h1>
-</body>
-</html>
-<form action="action_page.php" method="POST">
-    <table border="1">
-        <tr>
-            <td> Name: </td>
-            <td> <input type="text" name="name"> </td>
-        </tr>
-        <tr>
-            <td> Gender: </td>
-            <td>
-                <input type="radio" name="gender" value="male"> Male
-                <input type="radio" name="gender" value="female"> Female
-            </td>
-        </tr>
-        <tr>
-            <td> Country: </td>
-            <td>
-                <select name="country">
-                    <option value="PH">Philippines</option>
-                    <option value="AUS">Australia</option>
-                    <option value="US">United States</option>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <input type="submit" value="Save">
-            </td>
-        </tr>
-    </table>
-</form>
+<?php
+   var_dump($_POST);
+   
+   $FullName='';
+   $Gender='';
+   $Country='';
+?>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database="kurt_salonga_i201";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<?php
+$sql = "INSERT INTO Student_Record (ID, Full_Name, Gender, Country)
+VALUES ('', '$FullName', '$Gender', '$Country')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
--- register.php
+++ register.php
@@ -9,23 +9,23 @@
     <h1>REGISTER HERE!</h1>
 </body>
 </html>
-<form action="action.php" method="POST">
+<form action="action_page.php" method="POST">
     <table border="1">
         <tr>
             <td> Name: </td>
-            <td> <input type="text" name="name"> </td>
+            <td> <input type="Text" name="Name"> </td>
         </tr>
         <tr>
             <td> Gender: </td>
             <td>
-                <input type="radio" name="gender" value="male"> Male
-                <input type="radio" name="gender" value="female"> Female
+                <input type="Radio" name="Gender" value="Male"> Male
+                <input type="Radio" name="Gender" value="Female"> Female
             </td>
         </tr>
         <tr>
             <td> Country: </td>
             <td>
-                <select name="country">
+                <select name="Country">
                     <option value="PH">Philippines</option>
                     <option value="AUS">Australia</option>
                     <option value="US">United States</option>
@@ -34,7 +34,7 @@
         </tr>
         <tr>
             <td colspan="2">
-                <input type="submit" value="Save">
+                <input type="Submit" name="Submit" value="Submit">
             </td>
         </tr>
     </table>
