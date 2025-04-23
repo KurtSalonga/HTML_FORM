62330a9afd4dbabe90683011a0383a0cb0a32ecf
KurtSalonga <kasalonga23-0391@cca.edu.ph>
2025-04-23 18:27:53 +0800

    ACTION_PAGE.PHP
    UPDATED ACTION_PAGE.PHP FILE

--- action_page.php
+++ action_page.php
@@ -1,19 +1,22 @@
 <?php
+   // Display POST data for debugging
    echo "<pre>";
    var_dump($_POST);
-   echo "<pre></pre>";
+   echo "</pre>";
    echo "<br><br>";
 
+   // Get the form data
    $Full_Name = $_POST['Name'];
    $Gender = $_POST['Gender'];
    $Country = $_POST['Country'];
 ?>
 
 <?php
+// Database connection settings
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database="kurt_salonga_i201";
+$database = "kurt_salonga_i201";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -22,18 +25,22 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br>";
 ?>
 
 <?php
-$sql = "INSERT INTO Student_Record (ID, Full_Name, Gender, Country)
-VALUES ('', '$Full_Name', '$Gender', '$Country')";
+// Prepare SQL query to insert data securely using prepared statements
+$sql = $conn->prepare("INSERT INTO Student_Record (Full_Name, Gender, Country) VALUES (?, ?, ?)");
+$sql->bind_param("sss", $Full_Name, $Gender, $Country); // "sss" means three strings
 
-if ($conn->query($sql) === TRUE) {
+// Execute query and check for success
+if ($sql->execute() === TRUE) {
   echo "New record created successfully";
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql->error;
 }
 
+// Close connection
+$sql->close();
 $conn->close();
 ?>
